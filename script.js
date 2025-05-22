031653bb3511afa9999c4be6b39a7f96d3f15434
alumno <alumno@Instituto.local>
2025-05-22 11:16:00 -0300

    22/5

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ console.log(nombre.length)
 //2
 nombre.push("luciano", "federico")
 console.log(nombre.length)
-*/
+
 
 //EJERCICIO 3
 
@@ -39,3 +39,30 @@ let numeros = ["10", "50", "5"]
 for (let indice = 0; indice < numeros.length; indice++){
   console.log(numeros[indice]) 
 }
+
+//EJERCICIO 4
+
+//1
+let numbers = [20, 25, 30]
+
+//2
+let suma = 0
+for (let indice = 0; indice < numbers.length; indice++){
+  suma = suma + numbers[indice]
+}
+
+//3
+console.log("suma de lementos", suma)
+*/
+
+//EJERCICIO 5
+
+//1
+let arreglo = []
+
+for (let index = 1; index < 6; index++) {
+  arreglo.push(prompt("escibe palabra N°" + index))
+  
+}
+
+console.log(arreglo[index])

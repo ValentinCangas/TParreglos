7b1e59d0c76e7246226f743fd479afdca3e2ff2a
alumno <alumno@Instituto.local>
2025-06-11 08:10:59 -0300

    nuevo tp

--- script.js
+++ script.js
@@ -1,68 +1,30 @@
 
 //EJERCICIO 1
 
-//1
-/*
-let frutas = ["manzana", "sandia", "ananá"]
-console.log(frutas)
-//2
- frutas.push("banana");
-console.log(frutas)
-//3
-frutas.unshift("naranja")
-console.log(frutas)
-//4
-frutas.pop("banana")
-console.log(frutas)
-//5
-frutas.shift("naranja")
-console.log(frutas)
-*/
+/*let animales = ["perro", "gato", "conejo", "loro"]
+
+for (let i = 0; i < animales.length; i++) {
+  console.log(animales[i])
+}
+
 //EJERCICIO 2
 
-//1
-/*
-let nombre = ["agustin", "bautista", "maximo"]
-console.log(nombre.length)
- 
-//2
-nombre.push("luciano", "federico")
-console.log(nombre.length)
-
+for (let i = 0; i < animales.length; i=i+2) {
+  console.log(animales[i])
+}
 
 //EJERCICIO 3
 
-//1
-let numeros = ["10", "50", "5"]
+let numeros = [4, 7, 2, 9, 5];
 
-//2
-for (let indice = 0; indice < numeros.length; indice++){
-  console.log(numeros[indice]) 
+let suma = 0
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i]
 }
+console.log("El total es " + suma)
 
 //EJERCICIO 4
 
-//1
-let numbers = [20, 25, 30]
+let datos = [3, 6, 1, 8];*/
 
-//2
-let suma = 0
-for (let indice = 0; indice < numbers.length; indice++){
-  suma = suma + numbers[indice]
-}
-
-//3
-console.log("suma de lementos", suma)
-*/
-
-//EJERCICIO 5
-
-//1
-let arreglo = []
-
-for (let index = 1; index < 6; index++) {
-  arreglo.push(prompt("escibe palabra N°" + index))
-  
-}
-
-console.log(arreglo[index])
